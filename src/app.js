e1a0612eca0bf1f6b2e9135a027d4bab4f63c9b9
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-04-02 16:29:48 +0000

    proyecto completado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomPronoun = Math.floor(Math.random()*pronoun.length);
+  let randomAdj = Math.floor(Math.random()*adj.length);
+  let randomNoun = Math.floor(Math.random()*noun.length);
+  let result = pronoun[randomPronoun] + adj[randomAdj] + noun[randomNoun] + ".com";
+  alert(result);
+  console.log(result);
 };
